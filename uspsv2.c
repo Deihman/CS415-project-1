791cbd65e80a51c56b2db7bed94e4217500d5156
Deihman <clownvant@icloud.com>
2023-04-30 14:34:05 -0700

    in progress

--- uspsv2.c
+++ uspsv2.c
@@ -18,20 +18,23 @@
 #define MAXPROGRAMS 256
 #define MAXARGS 256
 
+enum {p1stdin = 0, p1stdout = 1, p1stderr = 2};
+
 void waitforSIGUSR1()
 {
+        int sig = SIGUSR1;
         sigset_t set;
         sigemptyset(&set);
 
         if (sigaddset(&set, SIGUSR1) == -1)
         {
-                p1perror(stderr, "sigaddset error");
+                p1perror(p1stderr, "sigaddset error");
                 _exit(1);
         }
 
-        if (sigwait(&set) != SIGUSR1)
+        if (sigwait(&set, &sig) != SIGUSR1)
         {
-                p1perror(stderr, "sigwait error");
+                p1perror(p1stderr, "sigwait error");
                 _exit(1);
         }
 }
@@ -41,14 +44,13 @@ void main(int argc, char** argv)
 {
         extern char* optarg;
         extern int optind;
-        enum {stdin = 0, stdout = 1, stderr = 2};
 
         int opt;
         int q = 0;
         int quantum = -1;
         char* filename = NULL;
         int checkstdin = 0;
-        int fd = stdin;
+        int fd = p1stdin;
 
         char* p;
         if ((p = getenv("USPS_QUANTUM_MSEC")) != NULL)
@@ -64,7 +66,7 @@ void main(int argc, char** argv)
                                 break;
 
                         case '?':
-                                p1putstr(stderr, "usage: ./uspsv? [-q <quantum in msec>] [workload_file]\n");
+                                p1putstr(p1stderr, "usage: ./uspsv? [-q <quantum in msec>] [workload_file]\n");
                                 _exit(1);
                                 break;
                 }
@@ -72,7 +74,7 @@ void main(int argc, char** argv)
 
         if (quantum == -1)
         {
-                p1putstr(stderr, "Invalid quantum value\n");
+                p1putstr(p1stderr, "Invalid quantum value\n");
                 _exit(1);
         }
 
@@ -83,7 +85,7 @@ void main(int argc, char** argv)
 
         if (optind < argc)
         {
-                p1putstr(stderr, "usage: ./uspsv? [-q <quantum in msec>] [workload_file]\n");
+                p1putstr(p1stderr, "usage: ./uspsv? [-q <quantum in msec>] [workload_file]\n");
                 _exit(1);
         }
 
@@ -92,7 +94,7 @@ void main(int argc, char** argv)
                 fd = open(filename, O_RDONLY);
                 if (fd == -1)
                 {
-                        p1putstr(stderr, "Unable to open workload file\n");
+                        p1putstr(p1stderr, "Unable to open workload file\n");
                         free(filename);
                         _exit(1);
                 }
@@ -119,42 +121,50 @@ void main(int argc, char** argv)
                         printf("word: %s\n", word);
                         if (numargs == 0)
                                 programs[numprograms] = p1strdup(word);
-                        else
-                                args[numprograms][numargs - 1] = p1strdup(word);
-                        
-                        numargs++;
+                        args[numprograms][numargs++] = p1strdup(word);
                 }
 
                 if (numargs == MAXARGS)
-                        p1putstr(stderr, "WARNING: argument buffer is full, continuing\n");
+                        p1putstr(p1stderr, "WARNING: argument buffer is full, continuing\n");
 
                 if (yeah)
-                {
-                        args[numprograms][numargs - 1] = NULL;
-                        numprograms++;
-                }
+                        args[numprograms++][numargs] = NULL;
         }
 
         if (numprograms == MAXPROGRAMS)
-                p1putstr(stderr, "WARNING: program buffer is full, continuing\n");
+                p1putstr(p1stderr, "WARNING: program buffer is full, continuing\n");
 
         printf("numprograms: %d\n", numprograms);
-        p1putstr(stdout, "Starting forks\n");
+        p1putstr(p1stdout, "Starting forks\n");
+
+        int parentpid = getpid();
 
         pid_t pid[MAXPROGRAMS];
         for (int i = 0; i < numprograms; i++)
         {
                 pid[i] = fork();
-                printf("pid %d forked to %d\n", getpid(), pid[i]);
+                if (pid[i] == 0)
+                        printf("Child process %d\n", getpid());
+                else
+                        printf("pid %d forked to %d\n", getpid(), pid[i]);
+
                 if (pid[i] == 0)
                 {
                         printf("child %d waiting\n", getpid());
-                        waitforSIGUSR1();
+                        //waitforSIGUSR1();
                         printf("child %d exec %s\n", getpid(), programs[i]);
-                        execvp(programs[i], args[i]);
+                        if (execvp(programs[i], args[i]) == -1)
+                        {
+                                p1putstr(p1stderr, "execvp failed\n");
+                                _exit(1);
+                        }
                         break;
                 }
         }
+
+        printf("Hello from pid %d\n", getpid());
+
+
 
         for (int i = 0; i < numprograms; i++)
         {
