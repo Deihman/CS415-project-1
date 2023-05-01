69983a7d59814ed787f9cb5944ba0dedcfca948a
Deihman <clownvant@icloud.com>
2023-04-30 19:20:09 -0700

    working

--- uspsv3.c
+++ uspsv3.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <signal.h>
@@ -21,17 +22,86 @@
 enum {p1stdin = 0, p1stdout = 1, p1stderr = 2};
 char* gp;
 char** ga;
-int gnp;
+int numprograms = 0;
+int numpids;
 pid_t pid[MAXPROGRAMS];
+int gactiveiter = -1;
+char* programs[MAXPROGRAMS];
+char* args[MAXPROGRAMS][MAXARGS+1];
+
+
+void cleanup()
+{
+        /* closers */
+        for(int i = 0; i < numprograms; i++)
+        {
+                free(programs[i]);
+                for (int j = 0; args[i][j] != NULL; j++)
+                {
+                        free(args[i][j]);
+                }
+        }
+
+        p1putstr(p1stdout, "exiting...\n");
+}
+
 
 void handler(int)
 {
-        printf("child %d exec %s\n", getpid(), gp);
         if (execvp(gp, ga) == -1)
         {
                 p1putstr(p1stderr, "execvp failed\n");
                 _exit(1);
         }
+}
+
+
+void alarmhandler(int)
+{
+        if (numpids == 0)
+        {
+                cleanup();
+                _exit(0);
+        }
+        if (gactiveiter != -1)
+                kill(pid[gactiveiter], SIGSTOP);
+        gactiveiter = (gactiveiter + 1) % numpids;
+
+        kill(pid[gactiveiter], SIGCONT);
+}
+
+
+void removeandshift(pid_t id)
+{
+        int i;
+        for (i = 0; i < numpids && pid[i] != id; i++)
+                ;
+        if (i == numpids)
+                return;
+        
+        for (int j = i; j < numpids-1; j++)
+        {
+                pid[j] = pid[j+1];
+        }
+        numpids--;
+}
+
+
+void deadchildhandler(int)
+{
+        pid_t p;
+
+        while ((p = waitpid(-1, NULL, WNOHANG)) > 0)
+        {
+                if (numpids == 0)
+                {
+                        cleanup();
+                        _exit(0);
+                }
+                
+                removeandshift(p);
+        }
+                
 }
 
 
@@ -87,10 +157,10 @@ void main(int argc, char** argv)
         if (filename != NULL)
         {
                 fd = open(filename, O_RDONLY);
+                free(filename);
                 if (fd == -1)
                 {
                         p1putstr(p1stderr, "Unable to open workload file\n");
-                        free(filename);
                         _exit(1);
                 }
         }
@@ -100,20 +170,14 @@ void main(int argc, char** argv)
         char buffer[BUFFLEN];
         char word[BUFFLEN];
 
-        char* programs[MAXPROGRAMS];
-        char* args[MAXPROGRAMS][MAXARGS+1];
-        int numprograms = 0;
-
         while (numprograms < MAXPROGRAMS && p1getline(fd, buffer, sizeof(buffer)) != 0)
         {
                 int yeah = 0;
                 int getworditer = 0;
                 int numargs = 0; /* per word */
-                printf("buffer: %s\n", buffer);
                 while (numargs < MAXARGS && (getworditer = p1getword(buffer, getworditer, word)) != -1)
                 {
                         yeah = 1;
-                        printf("word: %s\n", word);
                         if (numargs == 0)
                                 programs[numprograms] = p1strdup(word);
                         args[numprograms][numargs++] = p1strdup(word);
@@ -125,26 +189,22 @@ void main(int argc, char** argv)
                 if (yeah)
                         args[numprograms++][numargs] = NULL;
         }
+        if (fd != p1stdin)
+                close(fd);
 
-        gnp = numprograms;
+        numpids = numprograms;
 
         if (numprograms == MAXPROGRAMS)
                 p1putstr(p1stderr, "WARNING: program buffer is full, continuing\n");
 
-        printf("numprograms: %d\n", numprograms);
         p1putstr(p1stdout, "Starting forks\n");
 
         for (int i = 0; i < numprograms; i++)
         {
                 pid[i] = fork();
-                if (pid[i] == 0)
-                        printf("Child process %d\n", getpid());
-                else
-                        printf("pid %d forked to %d\n", getpid(), pid[i]);
 
                 if (pid[i] == 0)
                 {
-                        printf("child %d waiting\n", getpid());
                         gp = programs[i];
                         ga = args[i];
                         if (signal(SIGUSR1, handler) == SIG_ERR)
@@ -152,53 +212,36 @@ void main(int argc, char** argv)
                                 p1perror(p1stderr, "signal error\n");
                                 _exit(1);
                         }
-                        sleep(500);
+                        while (1) { pause(); }
                 }
         }
 
-        printf("Hello from pid %d\n", getpid());
+        if (signal(SIGALRM, alarmhandler) == SIG_ERR)
+        {
+                p1perror(p1stderr, "signal error\n");
+                _exit(1);
+        }
+
+        if (signal(SIGCHLD, deadchildhandler) == SIG_ERR)
+        {
+                p1perror(p1stderr, "signal error\n");
+                _exit(1);
+        }
 
         p1putstr(p1stdout, "waiting for children (5 sec)\n");
         sleep(5);
         for (int i = 0; i < numprograms; i++)
         {
-               printf("pid %d sending SIGUSR1 to pid %d\n", getpid(), pid[i]);
                kill(pid[i], SIGUSR1);
-        }
-
-        for (int i = 0; i < numprograms; i++)
-        {
-               printf("pid %d sending SIGSTOP to pid %d\n", getpid(), pid[i]);
                kill(pid[i], SIGSTOP);
         }
 
-        for (int i = 0; i < numprograms; i++)
-        {
-               printf("pid %d sending SIGCONT to pid %d\n", getpid(), pid[i]);
-               kill(pid[i], SIGCONT);
-        }
+        struct itimerval it;
+        it.it_interval.tv_sec = 0;
+        it.it_interval.tv_usec = quantum * 1000;
+        it.it_value.tv_sec = 0;
+        it.it_value.tv_usec = quantum* 1000;
+        setitimer(ITIMER_REAL, &it, NULL);
 
-        for (int i = 0; i < numprograms; i++)
-        {
-               printf("pid %d waiting on pid %d\n", getpid(), pid[i]);
-               wait(&pid[i]);
-        }
-
-        /* closers */
-        if (filename != NULL)
-        {
-                free(filename);
-                close(fd);
-        }
-
-        for(int i = 0; i < numprograms; i++)
-        {
-                free(programs[i]);
-                for (int j = 0; args[i][j] != NULL; j++)
-                {
-                        free(args[i][j]);
-                }
-        }
-
-        p1putstr(p1stdout, "exiting...\n");
+        while (1) { pause(); }
 }
